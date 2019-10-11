8f0bfc958576ec38b277b421ad233cf2dc314b8a
Valdo <im@osvaldopolii.com>
2019-10-11 16:51:07 +0800

    Add task functionality

--- app.js
+++ app.js
@@ -4,3 +4,40 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listener
+loadEventListeners();
+
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+
+// Add task
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    // Create li element
+    const li = document.createElement('li');
+    // Create class name for li
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create delete link
+    const link = document.createElement('a');
+    // Create class name for a
+    link.className = 'delete-item secondary-content';
+    // Create an icon for delete
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append a to li
+    li.appendChild(link);
+    // Append li to ul
+    taskList.appendChild(li);
+
+    // Clear input
+    taskInput.value = '';
+    console.log(li);
+
+    e.preventDefault();
+}
