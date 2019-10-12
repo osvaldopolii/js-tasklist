806ce69eaddeecaba12ac60d4e19185f8a234a7f
Valdo <im@osvaldopolii.com>
2019-10-12 10:50:08 +0800

    Finishing add task, delete task and clear tasks functionality

--- app.js
+++ app.js
@@ -6,38 +6,52 @@ const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 // Load all event listener
-loadEventListeners();
+loadEventListener();
 
-function loadEventListeners() {
-    form.addEventListener('submit', addTask);
+function loadEventListener(){
+    // Add task
+    document.addEventListener('submit', addTask);
+    // Remove task
+    document.addEventListener('click', removeTask);
+    // Clear task
+    clearBtn.addEventListener('click', clearTasks);
 }
 
-// Add task
-function addTask(e) {
-    if(taskInput.value === '') {
-        alert('Add a task');
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Please input the task');
+    } else {
+        // Create an element li
+        const li = document.createElement('li');
+        // Create a class name for li
+        li.className = 'collection-item';
+        // Create text node and append to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        // Create a delete link
+        const link = document.createElement('a');
+        // Create a class name for the delete link
+        link.className = 'delete-item secondary-content';
+        // Create delete icon
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // append link to li
+        li.appendChild(link);
+        // append li to ul
+        taskList.appendChild(li);
+
+        taskInput.value = '';    
+
+        e.preventDefault();
     }
+}
 
-    // Create li element
-    const li = document.createElement('li');
-    // Create class name for li
-    li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create delete link
-    const link = document.createElement('a');
-    // Create class name for a
-    link.className = 'delete-item secondary-content';
-    // Create an icon for delete
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // Append a to li
-    li.appendChild(link);
-    // Append li to ul
-    taskList.appendChild(li);
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log(e.target.parentElement.parentElement.remove());
+    }
+}
 
-    // Clear input
-    taskInput.value = '';
-    console.log(li);
-
-    e.preventDefault();
+function clearTasks(){
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
 }
